8bb36429573b8ab69f255c24ebd3ae57f305f979
vivvks <vivek322@gmail.com>
2017-07-30 23:02:36 +0530

    add helloword program

---
+++ TestJava.java
@@ -0,0 +1,10 @@
+package testProject;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("First jenkins project");
+	}
+
+}
